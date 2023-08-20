cc7666c4f863171aeb2dc73c79d51e5e73b0b015
Juan P. Travi <juantravi@hotmail.com>
2023-08-19 22:49:59 -0300

    antes del final

--- eventos.js
+++ eventos.js
@@ -17,22 +17,57 @@ const validarProductoEnCarrito  = (id) =>{
         producto.cantidad++
         //pinto la cantidad en el HTML
         const contarObjetosCarrito = carrito.reduce((acc,item) => item.cantidad+acc,0)
-        const cantidadCarrito = document.getElementById('cantidad-carrito')
-        cantidadCarrito.innerText=contarObjetosCarrito
+        const cantidadCarrito1 = document.getElementById('cantidad-carrito1')
+        const cantidadCarrito2 = document.getElementById('cantidad-carrito2')
+        cantidadCarrito1.innerText=contarObjetosCarrito
+        cantidadCarrito2.innerText=cantidadCarrito1.innerText
         guardarCarritoStorage(carrito)
     } else{     //producto nuevo en carrito
         const producto = productos.find(producto => producto.id ==id)
         carrito.push(producto)
         //pinto la cantidad en el HTML
         const contarObjetosCarrito = carrito.reduce((acc,item) => item.cantidad+acc,0)
-        const cantidadCarrito = document.getElementById('cantidad-carrito')
-        cantidadCarrito.innerText=contarObjetosCarrito
+        const cantidadCarrito1 = document.getElementById('cantidad-carrito1')
+        const cantidadCarrito2 = document.getElementById('cantidad-carrito2')
+        cantidadCarrito1.innerText=contarObjetosCarrito
+        cantidadCarrito2.innerText=cantidadCarrito1.innerText
         guardarCarritoStorage(carrito)
     }
 }
 
-const botonCarrito = document.getElementById('boton-carrito')
-botonCarrito.addEventListener('click', () => {
+const botonCarrito1 = document.getElementById('boton-carrito1')
+botonCarrito1.addEventListener('click', () => {
+    const contarObjetosCarrito = carrito.reduce((acc,item) => item.cantidad+acc,0)
+    Swal.fire({
+        title: `El carrito contiene ${contarObjetosCarrito} elementos`,
+        showDenyButton: true,
+        showCancelButton: true,
+        cancelButtonText: 'Seguir comprando',
+        confirmButtonText: 'Finalizar compra',
+        denyButtonText: `Vaciar`,
+        showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+          },
+          hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+          }, 
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+            carrito = []
+            guardarCarritoStorage(carrito)
+            cargarCarrito()
+          Swal.fire('Su compra se ha realizado con exito!', '', 'success')
+        } else if (result.isDenied) {
+            carrito = []
+            guardarCarritoStorage(carrito)
+            cargarCarrito()
+          Swal.fire('Su carrito ha sido vaciado', '', 'warning')
+        }
+      })
+})
+const botonCarrito2 = document.getElementById('boton-carrito2')
+botonCarrito2.addEventListener('click', () => {
     const contarObjetosCarrito = carrito.reduce((acc,item) => item.cantidad+acc,0)
     Swal.fire({
         title: `El carrito contiene ${contarObjetosCarrito} elementos`,
@@ -77,8 +112,10 @@ const guardarCarritoStorage = (carrito) => {
       carrito = obtenerCarritoStorage()
       //pinto la cantidad en el HTML
       const contarObjetosCarrito = carrito.reduce((acc,item) => item.cantidad+acc,0)
-      const cantidadCarrito = document.getElementById('cantidad-carrito')
-      cantidadCarrito.innerText=contarObjetosCarrito
+      const cantidadCarrito1 = document.getElementById('cantidad-carrito1')
+      const cantidadCarrito2 = document.getElementById('cantidad-carrito2')
+      cantidadCarrito1.innerText=contarObjetosCarrito
+      cantidadCarrito2.innerText=cantidadCarrito1.innerText
       
     }
   }
